02b9830f1a06e5dd850f59b87365eefa47a6b2e1
Andrey <andreyzharov91@yandex.ru>
2026-04-14 15:41:03 +0300

    fix mobile adaptive

--- app.js
+++ app.js
@@ -2,19 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const burgerButton = document.querySelector(".burger");
   const mobileMenu = document.querySelector(".mobile-menu");
 
-  const preloader = document.querySelector(".preloader");
-  preloader.classList.add("active");
-
-  const removePreloader = () => {
-    if (preloader) {
-      preloader.classList.remove("active");
-    }
-  };
-
   burgerButton.addEventListener("click", function () {
     burgerButton.classList.toggle("active");
     mobileMenu.classList.toggle("open");
   });
-
-  setTimeout(removePreloader, 1000);
 });
